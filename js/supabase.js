231f6b0e1da49570a642ced58e6cab44295177e4
FRANKLIN YIMY PEZO MONTESINOS <42632661@continental.edu.pe>
2026-09-22 19:08:47 -0500

    Initialize Supabase client in supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://mecwbchzkukpdarnpvae.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_nB9pJGfSENzRpc_mhts0WA_r711RvWo";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
